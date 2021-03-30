191e975246a536d31426129b5c85853e9bc967d2
Arafat Islam <30324265+pulak335@users.noreply.github.com>
2021-03-30 15:09:46 +0600

    Add files via upload

---
+++ nodejs-basic-project/project/lib/quotes/index.js
@@ -0,0 +1,22 @@
+
+
+// Dependencies
+const fs = require('fs');
+
+// Quotes object - Module scaffolding
+const quotes = {};
+
+// Get all the quotes and return them to the user
+quotes.allQuotes = function allQuotes() {
+    // Read the text file containing the quotes
+    const fileContents = fs.readFileSync(`${__dirname}/quotes.txt`, 'utf8');
+
+    // Turn the string into an array
+    const arrayOfQuotes = fileContents.split(/\r?\n/);
+
+    // Return the array
+    return arrayOfQuotes;
+};
+
+// Export the library
+module.exports = quotes;
